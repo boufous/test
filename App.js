2d2f4972bb55d389dc22da33a16cb74aef360fdf
Davos <toto@gmail.com>
2018-10-25 17:28:17 +0100

    toto

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 toto
-type Props = {};
+type Props = {}; 
 export default class App extends Component<Props> {
   render() {
     return (
